51db0a39324d98bf1cddc6e182a28f4a0e724b66
duyeong0020 <duyeong0020@gmail.com>
2025-03-26 12:58:28 +0900

    feat: Add Article entity with creation and update methods

---
+++ service/article/src/main/java/dudu/board/article/service/ArticleService.java
@@ -0,0 +1,43 @@
+package dudu.board.article.service;
+
+import dudu.board.article.entity.Article;
+import dudu.board.article.repository.ArticleRepository;
+import dudu.board.article.service.request.ArticleCreateRequest;
+import dudu.board.article.service.request.ArticleUpdateRequest;
+import dudu.board.article.service.response.ArticleResponse;
+import kuke.board.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final Snowflake snowflake = new Snowflake();
+    private final ArticleRepository articleRepository;
+
+    @Transactional
+    public ArticleResponse create(ArticleCreateRequest request) {
+        Article initArticle = Article.create(snowflake.nextId(), request.getTitle(), request.getContent(), request.getBoardId(), request.getWriterId());
+        Article article = articleRepository.save(initArticle);
+        return ArticleResponse.from(article);
+    }
+
+    @Transactional
+    public ArticleResponse update(Long articleId, ArticleUpdateRequest request) {
+        Article article = articleRepository.findById(articleId).orElseThrow();
+        article.update(request.getTitle(), request.getContent());
+        return ArticleResponse.from(article);
+    }
+
+    public ArticleResponse read(Long articleId) {
+        return ArticleResponse.from(articleRepository.findById(articleId).orElseThrow());
+    }
+
+    @Transactional
+    public void delete(Long articleId) {
+        articleRepository.deleteById(articleId);
+    }
+
+
+}
