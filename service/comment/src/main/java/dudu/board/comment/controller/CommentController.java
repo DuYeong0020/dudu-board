800ab3c695591b8ebd8924d21d4603b96d6d5f3d
duyeong0020 <duyeong0020@gmail.com>
2025-03-28 00:49:59 +0900

    feat: Implement CommentController for comment management endpoints

---
+++ service/comment/src/main/java/dudu/board/comment/controller/CommentController.java
@@ -0,0 +1,31 @@
+package dudu.board.comment.controller;
+
+import dudu.board.comment.service.CommentService;
+import dudu.board.comment.service.request.CommentCreateRequest;
+import dudu.board.comment.service.response.CommentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping("/v1/comments/{commentId}")
+    public CommentResponse read(
+            @PathVariable("commentId") Long commentId
+    ) {
+        return commentService.read(commentId);
+    }
+
+    @PostMapping("/v1/comments")
+    public CommentResponse create(@RequestBody CommentCreateRequest request) {
+        return commentService.create(request);
+    }
+
+    @DeleteMapping("/v1/comments/{commentId}")
+    public void delete(@PathVariable("commentId") Long commentId) {
+        commentService.delete(commentId);
+    }
+
+}
