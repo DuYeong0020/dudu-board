d7e6c1c39c6a23edf0875100cc708b84c5d2028e
duyeong0020 <duyeong0020@gmail.com>
2025-04-01 18:37:33 +0900

    feat: Integrate BoardArticleCountRepository for article creation and deletion tracking

--- service/article/src/main/java/dudu/board/article/service/ArticleService.java
+++ service/article/src/main/java/dudu/board/article/service/ArticleService.java
@@ -1,7 +1,9 @@
 package dudu.board.article.service;
 
 import dudu.board.article.entity.Article;
+import dudu.board.article.entity.BoardArticleCount;
 import dudu.board.article.repository.ArticleRepository;
+import dudu.board.article.repository.BoardArticleCountRepository;
 import dudu.board.article.service.request.ArticleCreateRequest;
 import dudu.board.article.service.request.ArticleUpdateRequest;
 import dudu.board.article.service.response.ArticlePageResponse;
@@ -18,11 +20,19 @@ import java.util.List;
 public class ArticleService {
     private final Snowflake snowflake = new Snowflake();
     private final ArticleRepository articleRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
 
     @Transactional
     public ArticleResponse create(ArticleCreateRequest request) {
         Article initArticle = Article.create(snowflake.nextId(), request.getTitle(), request.getContent(), request.getBoardId(), request.getWriterId());
         Article article = articleRepository.save(initArticle);
+        int result = boardArticleCountRepository.increase(request.getBoardId());
+        if (result == 0) {
+            boardArticleCountRepository.save(
+                    BoardArticleCount.init(request.getBoardId(), 1L)
+            );
+        }
+
         return ArticleResponse.from(article);
     }
 
@@ -39,7 +49,9 @@ public class ArticleService {
 
     @Transactional
     public void delete(Long articleId) {
-        articleRepository.deleteById(articleId);
+        Article article = articleRepository.findById(articleId).orElseThrow();
+        articleRepository.delete(article);
+        boardArticleCountRepository.decrease(article.getBoardId());
     }
 
     public ArticlePageResponse readAll(Long boardId, Long page, Long pageSize) {
@@ -60,6 +72,12 @@ public class ArticleService {
                 articleRepository.findAllInfiniteScroll(boardId, pageSize) :
                 articleRepository.findAllInfiniteScroll(boardId, pageSize, lastArticleId);
         return articles.stream().map(ArticleResponse::from).toList();
+    }
+
+    public Long count(Long boardId) {
+        return boardArticleCountRepository.findById(boardId)
+                .map(BoardArticleCount::getArticleCount)
+                .orElse(0L);
     }
 
 
