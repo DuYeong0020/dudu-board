bfb752598aed84ea3562f4abdda2eb077b2ac8dd
duyeong0020 <duyeong0020@gmail.com>
2025-04-04 12:43:41 +0900

    feat: Add event handlers for article creation, deletion, liking, unliking, viewing, and comment management

---
+++ service/hot-article/src/main/java/dudu/board/hotarticle/service/eventhandler/ArticleDeletedEventHandler.java
@@ -0,0 +1,35 @@
+package dudu.board.hotarticle.service.eventhandler;
+
+import dudu.board.hotarticle.repository.ArticleCreatedTimeRepository;
+import dudu.board.hotarticle.repository.HotArticleListRepository;
+import kuke.board.common.event.Event;
+import kuke.board.common.event.EventType;
+import kuke.board.common.event.payload.ArticleDeletedEventPayload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleDeletedEventHandler implements EventHandler<ArticleDeletedEventPayload> {
+
+    private final HotArticleListRepository hotArticleListRepository;
+    private final ArticleCreatedTimeRepository articleCreatedTimeRepository;
+
+
+    @Override
+    public void handle(Event<ArticleDeletedEventPayload> event) {
+        ArticleDeletedEventPayload payload = event.getPayload();
+        articleCreatedTimeRepository.delete(payload.getArticleId());
+        hotArticleListRepository.remove(payload.getArticleId(), payload.getCreatedAt());
+    }
+
+    @Override
+    public boolean supports(Event<ArticleDeletedEventPayload> event) {
+        return EventType.ARTICLE_DELETED == event.getType();
+    }
+
+    @Override
+    public Long findArticleId(Event<ArticleDeletedEventPayload> event) {
+        return event.getPayload().getArticleId();
+    }
+}
---
+++ service/hot-article/src/main/java/dudu/board/hotarticle/service/eventhandler/ArticleViewedEventHandler.java
@@ -0,0 +1,36 @@
+package dudu.board.hotarticle.service.eventhandler;
+
+import dudu.board.hotarticle.repository.ArticleViewCountRepository;
+import dudu.board.hotarticle.utils.TimeCalculatorUtils;
+import kuke.board.common.event.Event;
+import kuke.board.common.event.EventType;
+import kuke.board.common.event.payload.ArticleViewedEventPayload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleViewedEventHandler implements EventHandler<ArticleViewedEventPayload> {
+
+    private final ArticleViewCountRepository articleViewCountRepository;
+
+    @Override
+    public void handle(Event<ArticleViewedEventPayload> event) {
+        ArticleViewedEventPayload payload = event.getPayload();
+        articleViewCountRepository.createOrUpdate(
+                payload.getArticleId(),
+                payload.getArticleViewCount(),
+                TimeCalculatorUtils.calculateDurationToMidnight()
+        );
+    }
+
+    @Override
+    public boolean supports(Event<ArticleViewedEventPayload> event) {
+        return EventType.ARTICLE_VIEWED == event.getType();
+    }
+
+    @Override
+    public Long findArticleId(Event<ArticleViewedEventPayload> event) {
+        return event.getPayload().getArticleId();
+    }
+}
