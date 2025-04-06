3c44b8430f67677aa76c95503d64f973bb4eb8ef
duyeong0020 <duyeong0020@gmail.com>
2025-04-06 18:30:39 +0900

    feat: Update Spring Boot application configurations to use 'dudu.board' package

--- service/article/src/main/java/dudu/board/article/ArticleApplication.java
+++ service/article/src/main/java/dudu/board/article/ArticleApplication.java
@@ -2,8 +2,12 @@ package dudu.board.article;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EntityScan(basePackages = "dudu.board")
+@SpringBootApplication(scanBasePackages = "dudu.board")
+@EnableJpaRepositories(basePackages = "dudu.board")
 public class ArticleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ArticleApplication.class, args);
--- service/like/src/main/java/dudu/board/like/LikeApplication.java
+++ service/like/src/main/java/dudu/board/like/LikeApplication.java
@@ -2,8 +2,12 @@ package dudu.board.like;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EntityScan(basePackages = "dudu.board")
+@SpringBootApplication(scanBasePackages = "dudu.board")
+@EnableJpaRepositories(basePackages = "dudu.board")
 public class LikeApplication {
     public static void main(String[] args) {
         SpringApplication.run(LikeApplication.class, args);
