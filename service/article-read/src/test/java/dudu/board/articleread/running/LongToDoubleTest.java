c687234d8c734314d436986cde0561de4f515836
duyeong0020 <duyeong0020@gmail.com>
2025-04-10 16:24:52 +0900

    feat: Add ArticleIdListRepository and BoardArticleCountRepository for managing article IDs and counts in Redis

---
+++ service/article-read/src/test/java/dudu/board/articleread/running/LongToDoubleTest.java
@@ -0,0 +1,20 @@
+package dudu.board.articleread.running;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class LongToDoubleTest {
+
+    @Test
+    void longToDoubleTest() {
+        // long은 64비트로 정수
+        // double은 64비트로 부동소수점
+        long longValue = 111_111_111_111_111_111L;
+        System.out.println("longValue = " + longValue);
+        double doubleValue = longValue;
+        System.out.println("new BigDecimal(doubleValue).toString() = " + new BigDecimal(doubleValue).toString());
+        long longValue2 = (long) doubleValue;
+        System.out.println("longValue2 = " + longValue2);
+    }
+}
